6f77f533bed7ea1b27408718805d6d0ca74fe8ae
Filipp Trigub <44036741+PhilTrigu@users.noreply.github.com>
2023-07-12 10:11:28 +0200

    refactor terraform and ui

--- UI/lib/ui/common/app_strings.dart
+++ UI/lib/ui/common/app_strings.dart
@@ -1,2 +1,4 @@
+import 'package:test_stacked_web_app/ui/common/app_constants.dart';
+
 const String ksHomeBottomSheetTitle = 'Controls';
-const String ksHomeBottomSheetDescription = '';
+const String ksHomeBottomSheetDescription = 'Backend online at $backendEndpoint';
