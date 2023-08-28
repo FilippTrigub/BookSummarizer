a6d3a9953442b5435522e97908decf179f2ec131
Filipp Trigub <44036741+PhilTrigu@users.noreply.github.com>
2023-08-28 20:10:09 +0200

    fix forgotten lib

---
+++ README.md
@@ -0,0 +1,21 @@
+## Audio-Book-Summarizer
+
+No time to read, so you're listening to audiobooks? Me too. The problem: how to take notes?
+
+This is where this AI comes in. Just upload an MP3 or WAV file and a summary will be sent to your email address.
+
+Cherry on top? You get both a summary by chapter and an ultra-short summary for the entire book at no extra cost!
+
+For more information don't hesitate to reach out!
+
+_Filipp Trigub_
+
+## Technical specifications
+
+The bot uses OpenAI whisper for speech-to-text and GPT-3.5-16k generation.
+
+Flutter and Dart is used for the UI.
+
+Cloud Deployment and CI/CD is done with TerraForm and with GitHub to Azure. 
+
+A version is available on my [website](https://www.trigub.tech).
--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import threading
 import openai
-import uvicorn
 
 from datetime import datetime
 from typing import List
@@ -173,21 +172,21 @@ def set_up_params(user_input: UserInput):
     return transcription_path, book_summary_path, chapter_summary_path, costs_path
 
 
-if __name__ == "__main__":
-    log_info('Start App')
-    # uvicorn.run(app, host=HOST_IFACE, port=APP_PORT)
+# if __name__ == "__main__":
+#     log_info('Start App')
+#     uvicorn.run(app, host=HOST_IFACE, port=APP_PORT)
 
-    user_input = UserInput
-    user_input.title = 'test'
-    user_input.email = 'filipp.trigub@gmail.com'
-    user_input.file_name = 'test.mp3'
-    user_input.delimiters = ['test']
-    user_input.description = ' test'
-    user_input.name = 'test'
-    user_input.model_key = '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'
-    user_input.budget = '1'
-    send_mail(recipient_email=user_input.email,
-              subject='Your Summary is here!',
-              attachment_paths=['book_summaries\\2023_08_10_09_55_40_test.docx', 'chapter_summaries\\2023_08_10_09_55_40_test.docx'],
-              book_title='test')
+    # user_input = UserInput
+    # user_input.title = 'test'
+    # user_input.email = 'filipp.trigub@gmail.com'
+    # user_input.file_name = 'test.mp3'
+    # user_input.delimiters = ['test']
+    # user_input.description = ' test'
+    # user_input.name = 'test'
+    # user_input.model_key = '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'
+    # user_input.budget = '1'
+    # send_mail(recipient_email=user_input.email,
+    #           subject='Your Summary is here!',
+    #           attachment_paths=['book_summaries\\2023_08_10_09_55_40_test.docx', 'chapter_summaries\\2023_08_10_09_55_40_test.docx'],
+    #           book_title='test')
     # transcribe_and_summarize(user_input)
